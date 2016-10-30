86663e869158909d6e76223027b6e16b8af70f9d
sekut1626 <mobile.sekut@gmail.com>
2016-10-30 15:16:47 +0100

    first commit

---
+++ app/src/main/java/com/example/sebastian/mytodo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sebastian.mytodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_tasks:
+                break;
+
+            case R.id.action_projects:
+                break;
+
+            case R.id.action_notes:
+                break;
+
+            case R.id.action_statistic:
+                break;
+        }
+        return true;
+    }
+
+
+}
